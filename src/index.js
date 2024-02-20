4eee0ac04f5f56002bd70afbb04755d9759d3005
tpikachu <46133358+tpikachu@users.noreply.github.com>
2024-02-20 09:01:59 -0600

    dispatch with action library octokit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-const { Octokit } = require("@octokit/rest");
 const core = require("@actions/core");
+const github = require("@actions/github");
 const config = require("./config");
-
-const octokit = new Octokit({
-  auth: process.env.GH_TOKEN, // cycle this token as it's leaked
-});
 
 function setOutput(summary, shareLink) {
   core.setOutput("summary", summary);
@@ -14,19 +10,23 @@ function setOutput(summary, shareLink) {
 (async function () {
   console.log(config);
 
-  const dispatchResult = await octokit.rest.actions.createWorkflowDispatch({
-    owner: "testdriverdev",
-    repo: "testdriver",
-    workflow_id: "interpret-comment.yml",
-    ref: "test-bot",
-    inputs: {
-      repo: config.githubContext.owner + "/" + config.githubContext.repo,
-      issue: config.githubContext.issueNumber,
-      comment: config.input.prompt,
-      branch: config.githubContext.branch,
-      response: `I ran your tests and here's what I found:`,
-    },
-  });
+  const octokit = github.getOctokit(process.env.GH_TOKEN);
+
+  const dispatchResult = await octokit.request(
+    "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+    {
+      owner: "testdriverdev",
+      repo: "testdriver",
+      workflow_id: "interpret-comment.yml",
+      inputs: {
+        repo: config.githubContext.owner + "/" + config.githubContext.repo,
+        issue: config.githubContext.issueNumber,
+        comment: config.input.prompt,
+        branch: config.githubContext.branch,
+        response: `I ran your tests and here's what I found:`,
+      },
+    }
+  );
 
   console.log(dispatchResult);
 })();
