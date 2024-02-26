0741c6f30717c101db0f13dc4f0bbb6ef2004543
tpikachu <46133358+tpikachu@users.noreply.github.com>
2024-02-26 12:29:13 -0600

    use api

--- src/index.js
+++ src/index.js
@@ -1,126 +1,65 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const config = require("./config");
-import { v4 as uuidv4 } from "uuid";
-const UZip = require("uzip");
+const axios = require("axios");
+require("dotenv").config();
 
-function setOutput(summary, shareLink, conclusion) {
+function setOutput(summary, shareLink) {
   core.setOutput("summary", summary);
   core.setOutput("share-link", shareLink);
-  core.setOutput("conclusion", conclusion);
 }
 
 const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
 
 (async function () {
-  const owner = "replayableio";
-  const repo = "testdriver";
-  const branch = "michael/flag-based-action-or-bot";
-  const dispatchWorkflow = "interpret-comment.yml";
+  const baseUrl =
+    (process.env.IS_DEV
+      ? "https://replayable-dev-michael.ngrok.io"
+      : "https://replayable-api-production.herokuapp.com") + "/api/v1";
 
-  const octokit = github.getOctokit(process.env.GH_TOKEN);
+  const repo = process.env.IS_DEV
+    ? "replayableio/testdriver-action"
+    : config.githubContext.owner + "/" + config.githubContext.repo;
+  const branch = process.env.IS_DEV ? "main" : config.githubContext.branch;
+  const prompt = process.env.IS_DEV ? "open youtube" : config.input.prompt;
 
-  const dispatchId = uuidv4();
-
-  await octokit.request(
-    "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+  const {
+    data: { workflowId },
+  } = await axios.post(
+    `${baseUrl}/testdriver-dispatch`,
     {
-      owner,
       repo,
-      workflow_id: dispatchWorkflow,
-      ref: branch,
-      inputs: {
-        repo: config.githubContext.owner + "/" + config.githubContext.repo,
-        branch: config.githubContext.branch,
-        dispatchId,
-        comment: config.input.prompt,
-        isFromAction: "true",
-      },
+      branch,
+      prompt,
+    },
+    {
+      Accept: "application/json",
+      "Content-Type": "application/json",
     }
   );
 
-  const findWorkFlow = async () => {
-    const twoMinutesAgo = new Date();
-    twoMinutesAgo.setMinutes(twoMinutesAgo.getMinutes() - 2);
-
-    const workflowResponse = await octokit.request(
-      `GET /repos/{owner}/{repo}/actions/runs?created=>{time}&branch={branch}&event={trigger}`,
-      {
-        owner,
-        repo,
-        // Can just check dispatches in the past 2 minutes since we poll every 1 minute
-        time: twoMinutesAgo.toISOString(),
-        branch,
-        trigger: "workflow_dispatch",
-      }
-    );
-    const runs = workflowResponse.data.workflow_runs || [];
-
-    let workflowId;
-
-    for (let i in runs) {
-      const workflowRun = runs[i];
-
-      // fetch jobs for the workflow run
-      const jobsUrl = new URL(workflowRun.jobs_url);
-      const response = await octokit.request(`GET ${jobsUrl.pathname}`);
-      const jobs = response.data.jobs || [];
-
-      // iterate over jobs within workflow run
-      for (let j in jobs) {
-        const job = jobs[j];
-        const steps = job.steps;
-
-        if (steps.length > 1) {
-          // if after setup phase is a step that contains our
-          //  dispatch id then capture workflow details and break
-          if (steps[1].name == dispatchId) {
-            workflowId = job.run_id;
-            break;
-          }
-        }
-      }
-
-      // break iteration of workflow runs if workflow id found
-      if (workflowId) {
-        return workflowId;
-      }
-    }
-  };
-
-  const waitUntilWorkflowAvailable = async () => {
-    let workflowId;
-    while (!workflowId) {
-      await waitFor(1000 * 60);
-      workflowId = await findWorkFlow();
-    }
-
-    return workflowId;
-  };
-
-  const workflowId = await waitUntilWorkflowAvailable();
-
-  console.log("workflow found", workflowId);
+  console.log("workflow id:", workflowId);
 
   const checkStatus = async () => {
-    const workflow = await octokit.request(
-      "GET /repos/{owner}/{repo}/actions/runs/{run_id}",
+    const {
+      data: { status, conclusion },
+    } = await axios.post(
+      `${baseUrl}/testdriver-status-check`,
+      { workflowId },
       {
-        owner,
-        repo,
-        run_id: workflowId,
+        Accept: "application/json",
+        "Content-Type": "application/json",
       }
     );
 
-    return {
-      status: workflow.data.status,
-      conclusion: workflow.data.conclusion,
-    };
+    console.log("workflow status: ", status, conclusion);
+
+    return { status, conclusion };
   };
 
   const waitUntilComplete = async () => {
     let { status, conclusion } = await checkStatus();
-    while (status != "completed") {
+
+    while (status !== "completed") {
       await waitFor(1000 * 60 * 1);
       const resp = await checkStatus();
       status = resp.status;
@@ -130,44 +69,20 @@ const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
     return conclusion;
   };
 
-  const conclusion = await waitUntilComplete();
+  await waitUntilComplete();
 
-  console.log("completed with ", conclusion);
+  console.log("workflow completed");
 
-  // list workflow run artifacts
-  const artifacts = await octokit.request(
-    "GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts",
+  const {
+    data: { shareLink, oiResult },
+  } = await axios.post(
+    `${baseUrl}/testdriver-artifacts`,
+    { workflowId },
     {
-      owner,
-      repo,
-      run_id: workflowId,
+      Accept: "application/json",
+      "Content-Type": "application/json",
     }
   );
-
-  // find artifact id
-  let artifactId;
-  for (let i = 0; i < artifacts.data.total_count; i++) {
-    if (artifacts.data.artifacts[i].name === "share-link") {
-      artifactId = artifacts.data.artifacts[i].id;
-    }
-  }
-
-  // download the artifact
-  let downloadedArtifact = await octokit.request(
-    "GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}",
-    {
-      owner,
-      repo,
-      artifact_id: artifactId,
-      archive_format: "zip",
-    }
-  );
-
-  // get file data
-  let unzippedData = UZip.parse(downloadedArtifact.data);
-  const textDecoder = new TextDecoder();
-  const shareLink = textDecoder.decode(unzippedData["shareLink.txt"]);
-  const oiResult = textDecoder.decode(unzippedData["oiResult.txt"]);
 
   const isPassed = oiResult.includes("The test passed");
 
@@ -175,6 +90,6 @@ const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
     core.setFailed(oiResult);
   }
 
-  console.log(shareLink, oiResult, conclusion);
-  setOutput(shareLink, oiResult, conclusion);
+  console.log("results: ", shareLink, oiResult);
+  setOutput(shareLink, oiResult);
 })();
