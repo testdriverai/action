8ca70c60344f7be247c1b55948b8d962a372ae58
Ian Jennings <ian@meetjennings.com>
2025-01-13 20:54:01 -0600

    all we do is make a status

--- src/index.js
+++ src/index.js
@@ -46,219 +46,219 @@ axios.interceptors.response.use(
 );
 
 (async function () {
-  const baseUrl =
-    (process.env.IS_DEV
-      ? "http://localhost:1337"      
-      : "https://api.testdriver.ai") + "/api/v1";
+  // const baseUrl =
+  //   (process.env.IS_DEV
+  //     ? "http://localhost:1337"      
+  //     : "https://api.testdriver.ai") + "/api/v1";
 
-  const repo = process.env.IS_DEV
-    ? "replayableio/testdriver-action"
-    : config.githubContext.owner + "/" + config.githubContext.repo;
-  const branch = process.env.IS_DEV ? "main" : config.githubContext.branch;
+  // const repo = process.env.IS_DEV
+  //   ? "replayableio/testdriver-action"
+  //   : config.githubContext.owner + "/" + config.githubContext.repo;
+  // const branch = process.env.IS_DEV ? "main" : config.githubContext.branch;
 
-  let prerun = config.input.prerun;
-  let testdriverBranch = config.input.branch;
-  let key = config.input.key;
-  let os = config.input.os;
-  let testdriveraiVersion = config.input.version;
-  let createPR = config.input.createPR;
-  let prBranch = config.input.prBranch;
-  let prBase = config.input.prBase;
-  let prTitle = config.input.prTitle;
-  let prTestFilename = config.input.prTestFilename;
+  // let prerun = config.input.prerun;
+  // let testdriverBranch = config.input.branch;
+  // let key = config.input.key;
+  // let os = config.input.os;
+  // let testdriveraiVersion = config.input.version;
+  // let createPR = config.input.createPR;
+  // let prBranch = config.input.prBranch;
+  // let prBase = config.input.prBase;
+  // let prTitle = config.input.prTitle;
+  // let prTestFilename = config.input.prTestFilename;
 
-  console.log(`testdriver@${pgkVersion}`);
-  console.log(`testdriver-action@${testdriverBranch}`);
+  // console.log(`testdriver@${pgkVersion}`);
+  // console.log(`testdriver-action@${testdriverBranch}`);
 
-  let prompt = process.env.IS_DEV ? "open youtube" : config.input.prompt;
+  // let prompt = process.env.IS_DEV ? "open youtube" : config.input.prompt;
 
-  console.log("");
-  console.log(chalk.green("Inputs"));
-  console.log(chalk.yellow("repo:"), repo);
-  console.log(chalk.yellow("branch:"), branch);
-  console.log(chalk.yellow("os:"), os);
-  console.log(chalk.yellow("createPR:"), createPR);
-  if (createPR) {
-    if (prBranch) console.log(chalk.yellow("prBranch:"), prBranch);
-    if (prBase) console.log(chalk.yellow("prBase:"), prBase);
-    if (prTitle) console.log(chalk.yellow("prTitle:"), prTitle);
-    if (prTestFilename)
-      console.log(chalk.yellow("prTestFilename:"), prTestFilename);
-  }
-  console.log(chalk.yellow("prompt:"));
-  console.log(prompt);
-  console.log(chalk.yellow("prerun:"));
-  console.log(prerun);
-  console.log("");
+  // console.log("");
+  // console.log(chalk.green("Inputs"));
+  // console.log(chalk.yellow("repo:"), repo);
+  // console.log(chalk.yellow("branch:"), branch);
+  // console.log(chalk.yellow("os:"), os);
+  // console.log(chalk.yellow("createPR:"), createPR);
+  // if (createPR) {
+  //   if (prBranch) console.log(chalk.yellow("prBranch:"), prBranch);
+  //   if (prBase) console.log(chalk.yellow("prBase:"), prBase);
+  //   if (prTitle) console.log(chalk.yellow("prTitle:"), prTitle);
+  //   if (prTestFilename)
+  //     console.log(chalk.yellow("prTestFilename:"), prTestFilename);
+  // }
+  // console.log(chalk.yellow("prompt:"));
+  // console.log(prompt);
+  // console.log(chalk.yellow("prerun:"));
+  // console.log(prerun);
+  // console.log("");
 
-  console.log(chalk.green("TestDriver:"), '"Looking into it..."');
-  console.log(chalk.green("TestDriver:"), '"I can help ya test that!"');
+  // console.log(chalk.green("TestDriver:"), '"Looking into it..."');
+  // console.log(chalk.green("TestDriver:"), '"I can help ya test that!"');
 
-  const personalAccessToken = process.env.GITHUB_TOKEN || config.githubContext.token || undefined;
+  // const personalAccessToken = process.env.GITHUB_TOKEN || config.githubContext.token || undefined;
 
-  if (personalAccessToken) {
-    console.log(chalk.green("TestDriver:"), '"Access Token Supplied..."');
-  }
+  // if (personalAccessToken) {
+  //   console.log(chalk.green("TestDriver:"), '"Access Token Supplied..."');
+  // }
 
-  console.log(chalk.green("TestDriver:"), '"Starting my engine..."');
+  // console.log(chalk.green("TestDriver:"), '"Starting my engine..."');
 
-  const {
-    data: { dispatchId },
-  } = await axios.post(
-    `${baseUrl}/testdriver-dispatch`,
-    {
-      repo,
-      branch,
-      prompt,
-      os,
-      prerun,
-      version: testdriverBranch,
-      key,
-      os,
-      personalAccessToken,
-      testdriveraiVersion,
-      createPR,
-      prTitle,
-      prBase,
-      prBranch,
-      prTestFilename,
-    },
-    {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }
-  );
+  // const {
+  //   data: { dispatchId },
+  // } = await axios.post(
+  //   `${baseUrl}/testdriver-dispatch`,
+  //   {
+  //     repo,
+  //     branch,
+  //     prompt,
+  //     os,
+  //     prerun,
+  //     version: testdriverBranch,
+  //     key,
+  //     os,
+  //     personalAccessToken,
+  //     testdriveraiVersion,
+  //     createPR,
+  //     prTitle,
+  //     prBase,
+  //     prBranch,
+  //     prTestFilename,
+  //   },
+  //   {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   }
+  // );
 
-  console.log(chalk.green("TestDriver:"), '"3. 2. 1..."');
+  // console.log(chalk.green("TestDriver:"), '"3. 2. 1..."');
 
-  const checkWorkflow = async () => {
-    console.log(chalk.green("TestDriver:"), '"Launching..."');
+  // const checkWorkflow = async () => {
+  //   console.log(chalk.green("TestDriver:"), '"Launching..."');
 
-    const {
-      data: { workflowId },
-    } = await axios.post(
-      `${baseUrl}/testdriver-workflow`,
-      {
-        dispatchId,
-        branch: testdriverBranch,
-      },
-      {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      }
-    );
+  //   const {
+  //     data: { workflowId },
+  //   } = await axios.post(
+  //     `${baseUrl}/testdriver-workflow`,
+  //     {
+  //       dispatchId,
+  //       branch: testdriverBranch,
+  //     },
+  //     {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     }
+  //   );
 
-    return workflowId;
-  };
+  //   return workflowId;
+  // };
 
-  const waitUntilWorkflowAvailable = async () => {
-    let workflowId;
-    while (!workflowId) {
-      await waitFor(1000 * 60);
-      workflowId = await checkWorkflow();
-    }
+  // const waitUntilWorkflowAvailable = async () => {
+  //   let workflowId;
+  //   while (!workflowId) {
+  //     await waitFor(1000 * 60);
+  //     workflowId = await checkWorkflow();
+  //   }
 
-    return workflowId;
-  };
+  //   return workflowId;
+  // };
 
-  const workflowId = await waitUntilWorkflowAvailable();
+  // const workflowId = await waitUntilWorkflowAvailable();
 
-  const checkStatus = async () => {
-    let response = await axios.post(
-      `${baseUrl}/testdriver-status-check`,
-      { workflowId },
-      {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      }
-    );
+  // const checkStatus = async () => {
+  //   let response = await axios.post(
+  //     `${baseUrl}/testdriver-status-check`,
+  //     { workflowId },
+  //     {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     }
+  //   );
 
-    let status = response.data.status;
-    let conclusion = response.data.conclusion;
+  //   let status = response.data.status;
+  //   let conclusion = response.data.conclusion;
 
-    return { status, conclusion };
-  };
+  //   return { status, conclusion };
+  // };
 
-  console.log(chalk.green("TestDriver:"), '"Let\'s Go!!!"');
+  // console.log(chalk.green("TestDriver:"), '"Let\'s Go!!!"');
 
-  const waitUntilComplete = async () => {
-    let { status, conclusion } = await checkStatus();
+  // const waitUntilComplete = async () => {
+  //   let { status, conclusion } = await checkStatus();
 
-    while (status !== "completed") {
-      await waitFor(1000 * 60 * 1);
-      console.log(chalk.green("TestDriver:"), '"Testing..."');
-      const resp = await checkStatus();
-      status = resp.status;
-      conclusion = resp.conclusion;
-    }
+  //   while (status !== "completed") {
+  //     await waitFor(1000 * 60 * 1);
+  //     console.log(chalk.green("TestDriver:"), '"Testing..."');
+  //     const resp = await checkStatus();
+  //     status = resp.status;
+  //     conclusion = resp.conclusion;
+  //   }
 
-    return conclusion;
-  };
+  //   return conclusion;
+  // };
 
-  let conc = await waitUntilComplete();
+  // let conc = await waitUntilComplete();
 
-  console.log(chalk.green("TestDriver:"), chalk.green('"Done!"'));
-  console.log(chalk.green("TestDriver:"), "Writing my report...");
+  // console.log(chalk.green("TestDriver:"), chalk.green('"Done!"'));
+  // console.log(chalk.green("TestDriver:"), "Writing my report...");
 
-  const {
-    data: { shareLink, oiResult, exitcode },
-  } = await axios.post(
-    `${baseUrl}/testdriver-artifacts`,
-    { workflowId },
-    {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }
-  );
+  // const {
+  //   data: { shareLink, oiResult, exitcode },
+  // } = await axios.post(
+  //   `${baseUrl}/testdriver-artifacts`,
+  //   { workflowId },
+  //   {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   }
+  // );
 
-  console.log(chalk.green("TestDriver:"), "Interpreting results...");
+  // console.log(chalk.green("TestDriver:"), "Interpreting results...");
 
-  console.log("");
-  console.log("Test Report:");
-  if (conc === "failure") {
-    console.log(chalk.yellow("Workflow:"), chalk.red("Fail"));
-  } else {
-    console.log(chalk.yellow("Workflow:"), chalk.green("Pass"));
-  }
+  // console.log("");
+  // console.log("Test Report:");
+  // if (conc === "failure") {
+  //   console.log(chalk.yellow("Workflow:"), chalk.red("Fail"));
+  // } else {
+  //   console.log(chalk.yellow("Workflow:"), chalk.green("Pass"));
+  // }
 
-  const isPassed = parseInt(exitcode) === 0;
+  // const isPassed = parseInt(exitcode) === 0;
 
-  if (!isPassed) {
-    core.setFailed(oiResult);
-  }
+  // if (!isPassed) {
+  //   core.setFailed(oiResult);
+  // }
 
-  if (isPassed) {
-    console.log(chalk.yellow("Test:"), chalk.green("Pass"));
-  } else {
-    console.log(chalk.yellow("Test:"), chalk.red("Fail"));
-  }
+  // if (isPassed) {
+  //   console.log(chalk.yellow("Test:"), chalk.green("Pass"));
+  // } else {
+  //   console.log(chalk.yellow("Test:"), chalk.red("Fail"));
+  // }
 
-  let extractedFromMarkdown = extractLink(shareLink);
+  // let extractedFromMarkdown = extractLink(shareLink);
 
-  let gif = extractGif(shareLink); 
-  console.log(gif)
+  // let gif = extractGif(shareLink); 
+  // console.log(gif)
 
-  console.log("");
-  console.log(chalk.yellow("View Test Result on Dashcam.io:"));
+  // console.log("");
+  // console.log(chalk.yellow("View Test Result on Dashcam.io:"));
 
-  if (extractedFromMarkdown) {
-    console.log(extractedFromMarkdown);
-  } else {
-    console.log(chalk.red("Something went wrong with Dashcam"));
-    console.log(shareLink);
-  }
+  // if (extractedFromMarkdown) {
+  //   console.log(extractedFromMarkdown);
+  // } else {
+  //   console.log(chalk.red("Something went wrong with Dashcam"));
+  //   console.log(shareLink);
+  // }
 
-  console.log("");
-  console.log(chalk.yellow("TestDriver.ai Summary"));
-  console.log(oiResult);
+  // console.log("");
+  // console.log(chalk.yellow("TestDriver.ai Summary"));
+  // console.log(oiResult);
 
-  core.setOutput("summary", oiResult);
-  core.setOutput("link", extractedFromMarkdown);
-  core.setOutput("markdown", shareLink);
-  core.setOutput("success", isPassed);
+  // core.setOutput("summary", oiResult);
+  // core.setOutput("link", extractedFromMarkdown);
+  // core.setOutput("markdown", shareLink);
+  // core.setOutput("success", isPassed);
 
   // create a github check for this run
-  getOctokit(personalAccessToken).checks.create({
+  getOctokit(personalAccessToken).rest.checks.create({
     owner: config.githubContext.owner,
     repo: config.githubContext.repo,
     name: "TestDriver.ai",
@@ -267,47 +267,47 @@ axios.interceptors.response.use(
     conclusion: isPassed ? "success" : "failure",
     output: {
       title: "TestDriver.ai Results",
-      summary: oiResult,
-      text: extractedFromMarkdown,
-      details_url: extractedFromMarkdown,
+      summary: oiResult || 'demo',
+      text: extractedFromMarkdown || 'dmeo',
+      details_url: extractedFromMarkdown || 'demo',
       images: [
         {
           alt: "Dashcam.io Recording",
-          image_url: gif,
+          image_url: gif || 'https://i.sstatic.net/SYZFJ.png?s=64',
         },
       ],
     },
   });
 
-  await core.summary
-    .addHeading("TestDriver.ai Results")
-    .addLink("View Dashcam.io Recording!", extractedFromMarkdown)
-    .addHeading("Summary")
-    .addRaw(oiResult)
-    .addSeparator()
-    .addRaw(shareLink)
-    .write();
+  // await core.summary
+  //   .addHeading("TestDriver.ai Results")
+  //   .addLink("View Dashcam.io Recording!", extractedFromMarkdown)
+  //   .addHeading("Summary")
+  //   .addRaw(oiResult)
+  //   .addSeparator()
+  //   .addRaw(shareLink)
+  //   .write();
 
   
-  await axios.post(
-    `${baseUrl}/testdriver-result-create`,
-    {
-      testSuite: config.githubContext.workflow,
-      runId: config.githubContext.run_id,
-      replayUrl: extractedFromMarkdown,
-      instructions: prompt,
-      repo: repo,
-      branch: config.githubContext.head_ref || config.githubContext.ref,
-      commit: config.githubContext.sha,
-      platform: os,
-      success: isPassed,
-      summary: oiResult,
-      version: testdriveraiVersion
-    },
-    {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }
-  );
+  // await axios.post(
+  //   `${baseUrl}/testdriver-result-create`,
+  //   {
+  //     testSuite: config.githubContext.workflow,
+  //     runId: config.githubContext.run_id,
+  //     replayUrl: extractedFromMarkdown,
+  //     instructions: prompt,
+  //     repo: repo,
+  //     branch: config.githubContext.head_ref || config.githubContext.ref,
+  //     commit: config.githubContext.sha,
+  //     platform: os,
+  //     success: isPassed,
+  //     summary: oiResult,
+  //     version: testdriveraiVersion
+  //   },
+  //   {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   }
+  // );
 
 })();
