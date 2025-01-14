637772b06e6f5be8a7480d182fba6bdfb3743694
Ian Jennings <ian@meetjennings.com>
2025-01-13 20:43:07 -0600

    package

--- dist/index.js
+++ dist/index.js
@@ -39978,6 +39978,7 @@ const core = __nccwpck_require__(2186);
 const config = __nccwpck_require__(4570);
 const axios = __nccwpck_require__(8757);
 const chalk = __nccwpck_require__(8818);
+const { getOctokit } = __nccwpck_require__(5438);
 
 (__nccwpck_require__(2437).config)();
 
@@ -39991,6 +39992,16 @@ function extractLink(markdownString) {
   } else {
     return null;
   }
+}
+
+// extract a gif from a markdown string
+function extractGif(markdownString) {
+  const regex = /!\[.*?\]\((.*?\.gif)\)/;
+  const match = markdownString.match(regex);
+  if (match?.length) {
+    return match[1];
+  }
+  return null;
 }
 
 const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
@@ -40200,6 +40211,9 @@ axios.interceptors.response.use(
 
   let extractedFromMarkdown = extractLink(shareLink);
 
+  let gif = extractGif(shareLink); 
+  console.log(gif)
+
   console.log("");
   console.log(chalk.yellow("View Test Result on Dashcam.io:"));
 
@@ -40218,6 +40232,28 @@ axios.interceptors.response.use(
   core.setOutput("link", extractedFromMarkdown);
   core.setOutput("markdown", shareLink);
   core.setOutput("success", isPassed);
+
+  // create a github check for this run
+  getOctokit(personalAccessToken).checks.create({
+    owner: config.githubContext.owner,
+    repo: config.githubContext.repo,
+    name: "TestDriver.ai",
+    head_sha: config.githubContext.sha,
+    status: "completed",
+    conclusion: isPassed ? "success" : "failure",
+    output: {
+      title: "TestDriver.ai Results",
+      summary: oiResult,
+      text: extractedFromMarkdown,
+      details_url: extractedFromMarkdown,
+      images: [
+        {
+          alt: "Dashcam.io Recording",
+          image_url: gif,
+        },
+      ],
+    },
+  });
 
   await core.summary
     .addHeading("TestDriver.ai Results")
