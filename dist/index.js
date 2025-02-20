aaed69e5df02f6c101c2c468dc6a1dd1ab3e343b
Ian Jennings <ian@meetjennings.com>
2025-02-19 18:29:51 -0600

    fix for supplying branch

--- dist/index.js
+++ dist/index.js
@@ -41263,8 +41263,8 @@ axios.interceptors.response.use(
   let prTestFilename = config.input.prTestFilename;
 
   console.log(chalk.green("Version"));
-  console.log(`testdriver@${pgkVersion}`);
-  console.log(`testdriver-action@${testdriverBranch}`);
+  console.log(`testdriver@${testdriveraiVersion}`);
+  console.log(`testdriver-action@${pgkVersion}`);
 
   let prompt = process.env.IS_DEV ? "open youtube" : config.input.prompt;
 
@@ -41310,7 +41310,7 @@ axios.interceptors.response.use(
       prompt,
       os,
       prerun,
-      version: testdriverBranch,
+      version: testdriveraiVersion,
       key,
       os,
       personalAccessToken,
@@ -41352,7 +41352,7 @@ axios.interceptors.response.use(
   const waitUntilWorkflowAvailable = async () => {
     let workflowId;
     while (!workflowId) {
-      await waitFor(1000 * 60);
+      await waitFor(1000 * 60 * 2);
       workflowId = await checkWorkflow();
     }
 
@@ -41454,15 +41454,19 @@ axios.interceptors.response.use(
   core.setOutput("markdown", shareLink);
   core.setOutput("success", isPassed);
 
-  let res1 = await octokit.rest.repos.createCommitStatus({
-    owner: config.githubContext.owner,
-    repo: config.githubContext.repo,
-    sha: config.githubContext.sha,
-    state: isPassed ? "success" : "failure",
-    target_url: extractedFromMarkdown,
-    description: prompt.length > 127 ? prompt.substring(0, 127) + "..." : prompt,
-    context: "TestDriver.ai",
-  });
+  try {
+    await octokit.rest.repos.createCommitStatus({
+      owner: config.githubContext.owner,
+      repo: config.githubContext.repo,
+      sha: config.githubContext.sha,
+      state: isPassed ? "success" : "failure",
+      target_url: extractedFromMarkdown,
+      description: prompt.length > 127 ? prompt.substring(0, 127) + "..." : prompt,
+      context: "TestDriver.ai",
+    });
+  } catch (error) {
+    console.error("Failed to create commit status:", error);
+  }
 
   await core.summary
     .addHeading("TestDriver.ai Results")
@@ -41473,7 +41477,6 @@ axios.interceptors.response.use(
     .addRaw(shareLink)
     .write();
 
-  
   await axios.post(
     `${baseUrl}/testdriver-result-create`,
     {
